f226fd57dd9d1c13627792036934f16a900237b8
usamahafiz8 <usamahafiz8@gmail.com>
2023-06-19 20:22:10 +0500

    resolve error

--- src/components/searchQuery.tsx
+++ src/components/searchQuery.tsx
@@ -1,9 +1,136 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import TextField from "@mui/material/TextField";
+// import Autocomplete, {
+//   AutocompleteInputChangeReason,
+// } from "@mui/material/Autocomplete";
+// import { top100Films } from "../content/seachResultRelated";
+
+// export const QuerySearch: React.FC = () => {
+//   const [selectedItem, setSelectedItem] = useState<string | null>(null);
+//   const [code, setCode] = useState<string | undefined>(undefined);
+//   const [suggestions, setSuggestions] = useState<string[]>([]);
+//   const [previousSearches, setPreviousSearches] = useState<string[]>([]);
+
+//   console.log(suggestions);
+
+//   const handleSelectionChange = (
+//     event: React.ChangeEvent<{}>,
+//     value: string | null
+//   ) => {
+//     setSelectedItem(value);
+//     if (value) {
+//       const selectedFilm = top100Films.find((film : any) => film.docstring === value);
+//       if (selectedFilm) {
+//         setCode(selectedFilm.code);
+//       } else {
+//         setCode("");
+//       }
+
+//       setPreviousSearches((prevSearches) => [...prevSearches, value]);
+//     } else {
+//       setCode(undefined);
+//     }
+//   };
+
+//   const handleInputChange = (
+//     event: React.SyntheticEvent<Element, Event>,
+//     value: string,
+//     reason: AutocompleteInputChangeReason
+//   ) => {
+//     const input = value.toLowerCase();
+//     const filteredSuggestions: any= top100Films
+//       .map((option: any) => option.docstring)
+//       .filter((docstring) => docstring && docstring.toLowerCase().includes(input));
+//     setSuggestions(filteredSuggestions);
+//   };
+
+//   return (
+//     <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+//       {/* search area */}
+//       <div
+//         style={{
+//           display: "flex",
+//           flexDirection: "column",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <div style={{ width: "50%" }}>
+//           <Autocomplete
+//             freeSolo
+//             id="free-solo-2-demo"
+//             disableClearable
+//             options={suggestions}
+//             onChange={handleSelectionChange}
+//             onInputChange={handleInputChange}
+//             renderInput={(params) => (
+//               <TextField
+//                 {...params}
+//                 label="Search your Query"
+//                 InputProps={{
+//                   ...params.InputProps,
+//                   type: "search",
+//                 }}
+//               />
+//             )}
+//           />
+//         </div>
+//       </div>
+//       {/* show previous searches */}
+//       <div>
+//         {selectedItem && (
+//           <>
+//             <b>History</b>
+//             <div
+//               style={{
+//                 width: "100%",
+//                 display: "flex",
+//                 flexDirection: "row",
+//                 flexWrap: "wrap",
+//                 gap: "4px",
+//               }}
+//             >
+//               {previousSearches.map((search, index) => (
+//                 <p
+//                   key={index}
+//                   style={{
+//                     backgroundColor: "lightgreen",
+//                     borderRadius: "16px",
+//                     padding: 8,
+//                     color: "green",
+//                     margin: 3,
+//                   }}
+//                 >
+//                   {search}
+//                 </p>
+//               ))}
+//             </div>
+//           </>
+//         )}
+//       </div>
+//       {/* show related code */}
+//       <div>
+//         {code && (
+//           <>
+//             <b>Code:</b>
+//             <pre>{code}</pre>
+//           </>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+
+
+
+import React, { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete, {
   AutocompleteInputChangeReason,
 } from "@mui/material/Autocomplete";
 import { top100Films } from "../content/seachResultRelated";
+
 
 export const QuerySearch: React.FC = () => {
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
@@ -19,12 +146,11 @@ export const QuerySearch: React.FC = () => {
   ) => {
     setSelectedItem(value);
     if (value) {
-      const selectedFilm = top100Films.find((film) => film.docstring === value);
+      const selectedFilm = top100Films.find((film:any) => film.docstring === value);
       if (selectedFilm) {
         setCode(selectedFilm.code);
-      }
-      else{
-        setCode('')
+      } else {
+        setCode("");
       }
 
       setPreviousSearches((prevSearches) => [...prevSearches, value]);
@@ -37,15 +163,48 @@ export const QuerySearch: React.FC = () => {
     reason: AutocompleteInputChangeReason
   ) => {
     const input = value.toLowerCase();
-    const filteredSuggestions = top100Films
-      .map((option) => option.docstring)
-      .filter((docstring) => docstring.toLowerCase().includes(input));
-    setSuggestions(filteredSuggestions);
+    const filteredResults = top100Films.filter((film:any) =>
+      film.docstring.toLowerCase().includes(input)
+    );
+    const matchingStrings = filteredResults.map((film:any) => film.docstring);
+    const matchingCode = filteredResults.map((film:any) => film.code);
+    setSuggestions(matchingStrings);
+    setCode(matchingCode.join("\n")); // Combine relevant code into a single paragraph
   };
+
+
+
+// setting a log of the json file 
+const [data, setData] = useState<any[]>([]);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("bscs-2019-2023\src\content\test1.jsonl");
+      const content = await response.text();
+      const lines = content.split("\n");
+      const dataArray = lines.map((line) => {
+        const jsonData = JSON.parse(line);
+        console.log(jsonData, 'file'); // Log the parsed JSON object
+        return jsonData;
+      });
+      setData(dataArray);
+    } catch (error) {
+      console.error("Error fetching JSONL file:", error);
+    }
+  };
+const test = "bscs-2019-2023\src\content\test1.jsonl"
+  console.log('----',  test);
+  
+  fetchData();
+}, []);
+
+
+
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
-      {/* search aera */}
+      {/* search area */}
       <div
         style={{
           display: "flex",
@@ -75,10 +234,10 @@ export const QuerySearch: React.FC = () => {
           />
         </div>
       </div>
-      {/* show previos search */}
+      {/* show previous search */}
       <div>
-      {selectedItem && (
-        <>
+        {selectedItem && (
+          <>
             <b>History</b>
             <div
               style={{
@@ -88,48 +247,39 @@ export const QuerySearch: React.FC = () => {
                 flexWrap: "wrap",
                 gap: "4px",
               }}
-              >
+            >
               {previousSearches.map((search, index) => (
-                <>
-                  <p
-                    key={index}
-                    style={{
-                      backgroundColor: "lightgreen",
-                      borderRadius: "16px",
-                      padding: 8,
-                      color: "green",
-                      margin: 3,
-                    }}
-                  >
-                    {search},
-                  </p>
-                </>
+                <p
+                  key={index}
+                  style={{
+                    backgroundColor: "lightgreen",
+                    borderRadius: "16px",
+                    padding: 8,
+                    color: "green",
+                    margin: 3,
+                  }}
+                >
+                  {search},
+                </p>
               ))}
             </div>
           </>
-        ) }
-      </div>
-      {/* show related code */}
-      <div>
-        {code && (
-          <>
-            <b>Code:</b>
-            <pre>{code}</pre>
-          </>
         )}
       </div>
-      {/* {selectedItem && (
-        <>
-          <p>Selected Item: &nbsp; {selectedItem}</p>
-          {code && (
-            <>
-              <p>Code:</p>
-              <pre>{code}</pre>
-            </>
-          )}
-        </>
-      ) } */}
 
+      {/* show matching strings, URL, and code */}
+      {code && (
+        <div>
+          <b>Matching Strings, URL, and Code:</b>
+          {suggestions.map((matchingString, index) => (
+            <div key={index}>
+              <p>{matchingString}</p>
+              {/* <a href={top100Films[index].url}>{top100Films[index].url}</a> */}
+              <pre>{code[index]}</pre>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
